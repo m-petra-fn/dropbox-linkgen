82a3bf1f0c37675299b76d2db95e2c801faa08da
Francisco Ramos Bisneto <framos.df@gmail.com>
2025-02-11 17:31:47 -0300

    Primeiro commit

---
+++ src/main/java/br/com/petra/dropbox/linkgen/DropBoxLinkGenApplication.java
@@ -0,0 +1,13 @@
+package br.com.petra.dropbox.linkgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropBoxLinkGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropBoxLinkGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petra/dropbox/linkgen/DropBoxLinkGenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petra.dropbox.linkgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropBoxLinkGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
